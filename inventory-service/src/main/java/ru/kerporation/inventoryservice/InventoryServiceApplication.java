ef44682f93a6e9b18ac46cdb154539ad9680d33b
Kirill Kozlovskiy <k.kozlovskiy@paysend.com>
2023-10-04 21:51:33 +0300

    Eureka server

--- inventory-service/src/main/java/ru/kerporation/inventoryservice/InventoryServiceApplication.java
+++ inventory-service/src/main/java/ru/kerporation/inventoryservice/InventoryServiceApplication.java
@@ -3,11 +3,13 @@ package ru.kerporation.inventoryservice;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import ru.kerporation.inventoryservice.model.Inventory;
 import ru.kerporation.inventoryservice.repository.InventoryRepository;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class InventoryServiceApplication {
 
     public static void main(String[] args) {
