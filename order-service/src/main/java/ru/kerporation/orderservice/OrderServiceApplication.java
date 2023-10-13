345e36f038f04f1d7b1dc96eb2e9cc8b6e4353a4
Kirill Kozlovskiy <k.kozlovskiy@paysend.com>
2023-10-13 23:07:41 +0300

    Mongo last chance

--- order-service/src/main/java/ru/kerporation/orderservice/OrderServiceApplication.java
+++ order-service/src/main/java/ru/kerporation/orderservice/OrderServiceApplication.java
@@ -2,9 +2,10 @@ package ru.kerporation.orderservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {MongoAutoConfiguration.class})
 @EnableEurekaClient
 public class OrderServiceApplication {
 
